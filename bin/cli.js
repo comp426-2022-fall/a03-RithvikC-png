aed6f1602b6915251dae5421905485529f6a0c0c
RithvikC-png <rithvikc@ad.unc.edu>
2022-11-21 00:14:48 -0500

    Should be done

---
+++ bin/cli.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+import {roll} from "../lib/roll.js"
+import minimist from 'minimist';
+
+const args = minimist(process.argv.slice(2))
+
+let sides = args.sides;
+let dice = args.dice;
+let rolls = args.rolls;
+if(sides < 0 || dice < 0 || rolls < 0){
+    console.log("Please enter positive numbers");
+    process.exit(1);
+}
+let returnedObject = roll(sides, dice, rolls);
+console.log(returnedObject);
+process.exit(0);
